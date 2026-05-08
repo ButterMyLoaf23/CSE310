0ae0a87695fd1fb99c06599d784db64d469a7a53
ButterMyLoaf23 <bostonwyatt1@gmail.com>
2026-05-08 12:32:45 -0700

    I added a grid for the snake to move around in

--- Snake/snake.py
+++ Snake/snake.py
@@ -1,9 +1,10 @@
 import arcade
 import random
 
+grid_size = 20
 width = 800
 height = 600
-move_speed = 2.5
+move_speed = grid_size
 
 class Snake(arcade.Window):
 
@@ -18,11 +19,15 @@ class Snake(arcade.Window):
         self.snake_x = width // 2
         self.snake_y = height // 2
 
-        self.food_x = random.randint(0, width - 20)
-        self.food_y = random.randint(0, height - 20)
+        self.food_x = random.randrange(0, width, grid_size)
+        self.food_y = random.randrange(0, height, grid_size)
 
         self.snake_body = []
         self.snake_length = 1
+
+        self.set_update_rate(0.15)
+
+        self.game_over = False
 
     def on_draw(self):
         self.clear()
@@ -49,22 +54,32 @@ class Snake(arcade.Window):
 
 
     def on_update(self, delta_time):
+        if self.game_over: return
         self.snake_x += self.direction_x
         self.snake_y += self.direction_y
 
-        if (
-            abs(self.snake_x - self.food_x) < 20 
-            and abs(self.snake_y -self.food_y) < 20
-        ): 
-            self.food_x = random.randint(0, width - 20)
-            self.food_y = random.randint(0, height - 20)
+        if self.snake_x == self.food_x and self.snake_y == self.food_y: 
+            self.food_x = random.randrange(0, width, grid_size)
+            self.food_y = random.randrange(0, height, grid_size)
 
-            self.snake_length += 8
+            self.snake_length += 1
 
         self.snake_body.append((self.snake_x, self.snake_y))
         
         if len(self.snake_body) > self.snake_length:
             self.snake_body.pop(0)
+
+        for segment in self.snake_body[:-1]:
+            if segment == (self.snake_x, self.snake_y):
+                self.game_over = True
+
+        if (
+            self.snake_x < 0
+            or self.snake_x >= width
+            or self.snake_y < 0
+            or self.snake_y >= height
+        ):
+            self.game_over = True
 
     def on_key_press(self, key, modifiers):
 
