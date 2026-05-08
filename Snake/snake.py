ae23ad88a08811f8d1a15b4f36417bfd276012f6
ButterMyLoaf23 <bostonwyatt1@gmail.com>
2026-05-08 12:14:59 -0700

    I updated the food and my movement mechanics

--- Snake/snake.py
+++ Snake/snake.py
@@ -1,4 +1,5 @@
 import arcade
+import random
 
 width = 800
 height = 600
@@ -17,21 +18,53 @@ class Snake(arcade.Window):
         self.snake_x = width // 2
         self.snake_y = height // 2
 
+        self.food_x = random.randint(0, width - 20)
+        self.food_y = random.randint(0, height - 20)
+
+        self.snake_body = []
+        self.snake_length = 1
+
     def on_draw(self):
         self.clear()
 
-        arcade.draw_lrbt_rectangle_filled(
-            self.snake_x,
-            self.snake_x + 20,
-            self.snake_y,
-            self.snake_y + 20,
-            arcade.color.GREEN
+        for segment in self.snake_body:
+            arcade.draw_rect_filled(
+                arcade.XYWH(
+                    segment[0],
+                    segment[1],
+                    20, 20
+                ),
+                arcade.color.GREEN
+            )
+
+    
+        arcade.draw_rect_filled(
+            arcade.XYWH(
+                self.food_x,
+                self.food_y,
+                20, 20
+            ),
+            arcade.color.RED
         )
 
 
     def on_update(self, delta_time):
         self.snake_x += self.direction_x
         self.snake_y += self.direction_y
+
+        if (
+            abs(self.snake_x - self.food_x) < 20 
+            and abs(self.snake_y -self.food_y) < 20
+        ): 
+            self.food_x = random.randint(0, width - 20)
+            self.food_y = random.randint(0, height - 20)
+
+            self.snake_length += 8
+
+        self.snake_body.append((self.snake_x, self.snake_y))
+        
+        if len(self.snake_body) > self.snake_length:
+            self.snake_body.pop(0)
 
     def on_key_press(self, key, modifiers):
 
